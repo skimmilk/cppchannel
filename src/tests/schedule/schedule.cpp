9acd00354a023852d80bf240f628277671bf74e5
William DeGraaf <nswilliwa@gmail.com>
2014-09-28 01:58:04 -0500

    Let compiler determine template arguments and string cast in test

--- src/tests/schedule/schedule.cpp
+++ src/tests/schedule/schedule.cpp
@@ -6,12 +6,12 @@ using namespace csp;
 CSP_DECL(sleepsort, nothing, string, int)(int in)
 {
 	sleep(in);
-	put(string(std::to_string(in)));
+	put(std::to_string(in));
 }
 
 int main()
 {
 	std::vector<int> nums {3, 5, 1, 2, 4, 0};
-	vec(nums) | schedule<int, string>(sleepsort) | print();
+	vec(nums) | schedule(sleepsort) | print();
 	return 0;
 }
